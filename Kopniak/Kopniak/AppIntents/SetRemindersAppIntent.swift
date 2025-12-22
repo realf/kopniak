f0fa3a66f391664db011936c64c48000d04574e9
Serhii Dunets <dunets.devel@gmail.com>
2025-12-22 20:33:51 +0000

    clarify copies in SetRemindersAppIntent

--- Kopniak/Kopniak/AppIntents/SetRemindersAppIntent.swift
+++ Kopniak/Kopniak/AppIntents/SetRemindersAppIntent.swift
@@ -9,12 +9,12 @@ import AppIntents
 import ComposableArchitecture
 import Foundation
 
-enum RemindersStateSwitchOption: String, AppEnum {
+enum BreakRemindersAction: String, AppEnum {
     static let typeDisplayRepresentation: TypeDisplayRepresentation =
-        "Break Reminders State Switch Option"
+        "Break Reminders Action"
 
     static let caseDisplayRepresentations:
-        [RemindersStateSwitchOption: DisplayRepresentation] = [
+        [BreakRemindersAction: DisplayRepresentation] = [
             .start: DisplayRepresentation(title: "Start", image: .init(systemName: "play.fill")),
             .stop: DisplayRepresentation(title: "Stop", image: .init(systemName: "stop.fill")),
             .toggle: DisplayRepresentation(title: "Toggle", image: .init(systemName: "playpause.fill"))
@@ -28,27 +28,27 @@ enum RemindersStateSwitchOption: String, AppEnum {
 struct SetRemindersAppIntent: AppIntent {
     static let title: LocalizedStringResource = "Set Break Reminders"
     static let description: IntentDescription = IntentDescription(
-        "Opens the app and switches the state of break reminders."
+        "Opens the app and performs a selected action on the break reminders."
     )
     @AppDependency var store: StoreOf<AppIntentFeature>
 
     @Parameter(
-        title: "Start",
-        description: "The break reminders state switch option.",
+        title: "Break Reminders Action",
+        description: "Action to perform on the break reminders.",
         default: .start
     )
-    var stateTransitionOption: RemindersStateSwitchOption
+    var breakRemindesAction: BreakRemindersAction
 
     @available(macOS 26.0, *)
     static let supportedModes: IntentModes = [.background, .foreground]
 
     static var parameterSummary: some ParameterSummary {
-        Summary("\(\.$stateTransitionOption) break reminders")
+        Summary("\(\.$breakRemindesAction) break reminders")
     }
 
     @MainActor
     func perform() async throws -> some IntentResult {
-        switch stateTransitionOption {
+        switch breakRemindesAction {
         case .start:
             store.send(.delegate(.startReminders))
         case .stop:
